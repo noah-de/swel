16b36b89b64f1ed08072c0be42b837fcf8c5934c
Noah Spahn <noah.de@gmail.com>
2018-12-17 19:51:05 -0800

    continue to modularize

--- wave_swell.py
+++ wave_swell.py
@@ -19,37 +19,37 @@ def get_buoy_data(station = 46054):
     url = 'https://www.ndbc.noaa.gov/data/realtime2/{}.data_spec'.format(station)
     dest = './{}.data_spec'.format(station)
     urlretrieve(url, dest)
-    print('dest:',dest)
     return dest
 
+def read_data(dest):
+    dates = []
+    energies = []
+    frequencies = []
+
+    with open(dest) as fp:
+        for _ in range(3):
+            next(fp)
+
+        for l in fp:
+            dates.append(l.split()[0:5])
+
+            # [E] get wave energy data as E (meters squared per second)
+            energies.append([float(e) for e in l.split()[6::2]])
+
+            # [f] get frequency data as collection (1/seconds) measure of cycles per second
+            freqs=l.split()[7::2]
+            frequencies.append([float(i[1:-1]) for i in freqs])
+
+        fp.close()
+        # print("{}\t{}\t{}".format(len(dates), len(energies), len(frequencies)))
+
+        # convert the lists to numpy arrays
+        E = np.array(energies)    # E for 'Energy'
+        f = np.array(frequencies) # f for 'frequency'
+    return (E,f)
+
 dest = get_buoy_data(46053)
-
-dates = []
-energies = []
-frequencies = []
-
-with open(dest) as fp:
-    for _ in range(3):
-        next(fp)
-    
-    for l in fp:
-
-        dates.append(l.split()[0:5])
-        # separation_frequency = l.split()[5] # this value is ignored
-        
-        # [E] get wave energy data as E (meters squared per second)
-        energies.append([float(e) for e in l.split()[6::2]])
-
-        # [f] get frequency data as collection (1/seconds) measure of cycles per second
-        freqs=l.split()[7::2]
-        frequencies.append([float(i[1:-1]) for i in freqs])
-
-    fp.close()
-# print("{}\t{}\t{}".format(len(dates), len(energies), len(frequencies)))
-
-# convert the lists to numpy arrays
-E = np.array(energies)    # E for 'Energy'
-f = np.array(frequencies) # f for 'frequency'
+E, f = read_data(dest)
 
 # Average of frequency pairs comparing each frequency with the one after it
 df = np.diff(f)
@@ -67,14 +67,14 @@ SWHflipped = SWH[::-1]
 print(SWHflipped)
 
 # define period second intervals
-p = np.array([0,5,7,9,11,13,15,17,19,21,35])
-
-# period mid-point
-notfirst = p[1:]       # every element in a row, except for the first
-notlast  = p[:-1]      # every element in a row, except for the last
-pmid = .5*(notfirst + notlast)
-
-# shift the focus from frequencies to periods
-pf = 1./fmid
+# p = np.array([0,5,7,9,11,13,15,17,19,21,35])
+#
+# # period mid-point
+# notfirst = p[1:]       # every element in a row, except for the first
+# notlast  = p[:-1]      # every element in a row, except for the last
+# pmid = .5*(notfirst + notlast)
+#
+# # shift the focus from frequencies to periods
+# pf = 1./fmid
 
 #print("pf:\n",pf)
