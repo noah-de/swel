2403cbda21fd3e1cd189d83c76e5eed920a1d6aa
Noah Spahn <noah.de@gmail.com>
2018-12-20 06:06:51 -0800

    clean up the matlab code

--- wave_plot.m
+++ wave_plot.m
@@ -1,5 +1,10 @@
 %function H = get_wave_data;
 
+% data file explained: https://www.ndbc.noaa.gov/data_spec.shtml
+
+% spectral wave data derivations explained:
+%    https://www.ndbc.noaa.gov/wave.shtml
+
 % the address to the buoy data (should pass in as argument)
 % 46053 = E. Santa Barbara
 % 46054 = W. Santa Barbara
@@ -17,8 +22,7 @@ format = ['%{y}D%{m}D%{d}D%{hh}D%{mm}D%f' repmat('%f(%f)', [1 N])];
 c = textscan(fid, format,'HeaderLines',1);
 fclose(fid);
 
-% get frequency data (s^-1) 
-% 1/seconds measure of cycles per second
+% get separation frequency data (s^-1)
 n = length(c{8});
 f = zeros(n,N);
 for i = 1:N
@@ -26,7 +30,7 @@ for i = 1:N
     f(:,i) = c{iN};
 end
 
-% get wave energy data (m^2 s)
+% get Energy measurements for each frequency (m^2 s)
 % meters squared per second (Energy)
 E = zeros(n,N); % blank array
 for i = 1:N
@@ -34,22 +38,23 @@ for i = 1:N
     E(:,i) = c{iN};
 end
 
-% calculate df and Emid 
-% average of 
 df = diff(f,1,2); % compare this element with the one next to it, along the 2nd dimension
+
+% moving window comparison, mean average between elements
 Emid = .5*(E(:,1:N-1)+E(:,2:N));
 fmid = .5*(f(:,1:N-1)+f(:,2:N));
 
-% significant wave height (m -> multiply by 3.28 to get in feet)
-% integrating across the entire range of frequencies
-% (the integral)
-SWH = 4*sqrt(sum(df.*Emid,2)); % .* takes two arrays and returns a 3rd array (of same size) The '2' restricts this to the 2nd dimension
-% Why muntiply by 4?
+% significant wave height
+% .* takes two arrays and returns a 3rd array (of same size)
+% The '2' restricts this to the 2nd dimension
+SWH = 4*sqrt(sum(df.*Emid,2));
 
 % calculate significant wave height as a function of wave period
 % 'P' is for period (second intervals)
-P = [0,5,7,9,11,13,15,17,19,21,35];%25];%[5.5,6.5,7.5,8.5,9.5,10.5,11.5,13,15,19,22];
-Pmid = .5*(P(1:end-1)+P(2:end)); % calculating the mid-points for each period (makes the plots nicer)
+P = [0,5,7,9,11,13,15,17,19,21,35];
+
+% calculating the mid-points for each period (makes the plots nicer)
+Pmid = .5*(P(1:end-1)+P(2:end));
 Pf = 1./fmid(1,:); % shift the focus from frequencies to periods
 SWHmid = zeros(size(df, 1),length(Pmid)); % set aside space for this data set
 for i = 1:length(Pmid)
