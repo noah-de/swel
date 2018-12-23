57ca44dd2d4ea9e45bbc9783976e972a27c3278e
Noah Spahn <noah.de@gmail.com>
2018-12-23 13:17:05 -0800

    Black formatter

--- wave_swell.py
+++ wave_swell.py
@@ -1,20 +1,24 @@
 import numpy as np
 from urllib.request import urlretrieve
 
+
 def calc_midpoint(series):
-    nofirst = series[:,1:]       # every element in a row, except for the first
-    nolast  = series[:,:-1]      # every element in a row, except for the last
-    mid = .5 * (nolast + nofirst)
+    nofirst = series[:, 1:]  # every element in a row, except for the first
+    nolast = series[:, :-1]  # every element in a row, except for the last
+    mid = 0.5 * (nolast + nofirst)
     return mid
 
-def get_buoy_data(station = 46054):
-    url = 'https://www.ndbc.noaa.gov/data/realtime2/{}.data_spec'.format(station)
-    dest = './{}.data_spec'.format(station)
+
+def get_buoy_data(station=46054):
+    url = "https://www.ndbc.noaa.gov/data/realtime2/{}.data_spec".format(station)
+    dest = "./{}.data_spec".format(station)
     urlretrieve(url, dest)
     return dest
 
+
 def read_data(dest):
-    '''Read the data file from NDBC (NOAA) and put it into a usable data
+    """
+    Read the data file from NDBC (NOAA) and put it into a usable data
     structure
     https://www.ndbc.noaa.gov/data_spec.shtml
     @params:
@@ -22,7 +26,7 @@ def read_data(dest):
     @returns:
       Energy
       frequency
-    '''
+    """
     dates = []
     energies = []
     frequencies = []
@@ -32,65 +36,57 @@ def read_data(dest):
             next(fp)
 
         for l in fp:
-            # not using dates in matlab code, but it may be helpful here
             dates.append(l.split()[0:5])
-
-            # [E] get wave energy data as E (meters squared per second)
             energies.append([float(e) for e in l.split()[6::2]])
-
-            # [f] get frequency data as collection (1/seconds) measure of cycles per second
-            freqs=l.split()[7::2]
+            freqs = l.split()[7::2]
             frequencies.append([float(i[1:-1]) for i in freqs])
 
         fp.close()
         # print("{}\t{}\t{}".format(len(dates), len(energies), len(frequencies)))
 
-        # convert the lists to numpy arrays
-        E = np.array(energies)    # E for 'Energy'
-        f = np.array(frequencies) # f for 'frequency'
-    return (E,f)
+        E = np.array(energies)
+        f = np.array(frequencies)
+    return (E, f)
 
-def calc_swh(E,f):
-    # Average of frequency pairs comparing each frequency with the one after it
+
+def calc_swh(E, f):
     df = np.diff(f)
-
-    # calculate the energy mid-point
     Emid = calc_midpoint(E)
-
-    # calculate the frequency mid-point
-    fmid = calc_midpoint(f) # .5*(f[:,:-1] + f[:,1:])
+    fmid = calc_midpoint(f)
 
     # significant wave height
-    product = (df*Emid)
-    return 4*np.sqrt(product.sum(axis=1));
+    product = df * Emid
+    return 4 * np.sqrt(product.sum(axis=1))
+
 
 def get_swh(list):
-    '''
+    """
     params: list
     returns: dictionary that maps significant wave height data to a buoy number
-    '''
+    """
     locations = {}
 
     for buoy in list:
         dest = get_buoy_data(buoy)
         E, f = read_data(dest)
-        SWH = calc_swh(E,f)
+        SWH = calc_swh(E, f)
 
         SWHflipped = SWH[::-1]
         print(SWHflipped)
         locations[buoy] = SWHflipped
     return locations
 
+
 if __name__ == "__main__":
     buoys = [
-            46053, # E. Santa Barbara
-            46054, # W. Santa Barbara
-            46217, # Anacapa Passage
-            46086, # San Clemente Basin
-            46219, # San Nicolas Island
-            46025, # Santa Monica Basin
-            46069, # South Santa Rosa Island
-            ]
+        46053,  # E. Santa Barbara
+        46054,  # W. Santa Barbara
+        46217,  # Anacapa Passage
+        46086,  # San Clemente Basin
+        46219,  # San Nicolas Island
+        46025,  # Santa Monica Basin
+        46069,  # South Santa Rosa Island
+    ]
 
     locations = get_swh(buoys)
     print(type(locations))
@@ -98,6 +94,7 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import numpy as np
     import seaborn as sns
+
     sns.set_style("darkgrid")
     for b, data in locations.items():
         plt.plot(data)
@@ -115,4 +112,4 @@ if __name__ == "__main__":
 # # shift the focus from frequencies to periods
 # pf = 1./fmid
 
-#print("pf:\n",pf)
+# print("pf:\n",pf)
