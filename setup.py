485ad03a06d858e3c14d06e980b85a55a363f557
Noah Spahn <noah.de@gmail.com>
2018-12-30 18:49:02 -0800

    this can be installed with pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='swel',
+      version='0.1',
+      description='calculate significant wave height periods from raw NOAA buoy data',
+      url='https://github.com/noah-de/swel',
+      author='Noah Spahn',
+      author_email='noah.de@gmail.com	',
+      license='MIT',
+      packages=['swel'],
+      zip_safe=False)
