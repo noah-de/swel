b79387ea344cd19e8a2f9c66f9367a35f39d3b68
Noah Spahn <noah.de@gmail.com>
2019-01-15 19:02:33 -0800

    begin testing the Buoy class

--- swel/tests/buoy_test.py
+++ swel/tests/buoy_test.py
@@ -3,3 +3,13 @@ import unittest
 
 from swel.buoy import Buoy
 
+class Buoy_test(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.buoy = Buoy(46086)
+
+    # test case for the function file_search()
+    def test_file_search(self):
+        data = self.buoy.get_data()
+        assert data != None
