c836fce3a2bde9d5f74179eefde80e0183dc306e
Noah Spahn <noah.de@gmail.com>
2019-01-20 22:04:32 -0800

    add logging and consolidate into one class

--- swel/buoy.py
+++ swel/buoy.py
@@ -1,21 +1,25 @@
 import numpy as np
 from urllib.request import urlretrieve
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 URL = "https://www.ndbc.noaa.gov/data/realtime2/{}.data_spec"
-DEST = "./data/{}.data_spec"
+DEST = "data/{}.data_spec"
 
 
 class Buoy:
     def __init__(self, buoy):
+        logging.debug("initializing buoy")
         self.buoy = buoy
         self.dates = []
         self.E = np.array([])
         self.f = np.array([])
 
-    def get_data(self, dest=""):
+    def get_data(self, dest=None):
+        logging.debug("calling get_data()")
         url = URL.format(self.buoy)
-        if dest != "":
+        if dest is None:
             dest = DEST.format(self.buoy)
 
         urlretrieve(url, dest)
@@ -42,3 +46,18 @@ class Buoy:
             E = np.array(energies)
             f = np.array(frequencies)
         return (E, f)
+
+    def bootstrap(self, E, f):
+        self.Emid = self.calc_midpoint(E)
+        self.fmid = self.calc_midpoint(f)
+        self.df = np.diff(f)
+
+    def calc_swh(self):
+        product = self.df * self.Emid
+        return 4 * np.sqrt(product.sum(axis=1))
+
+    def calc_midpoint(self, series):
+        nofirst = series[:, 1:]  # every element in a row, not the first
+        nolast = series[:, :-1]  # every element in a row, not the last
+        mid = 0.5 * (nolast + nofirst)
+        return mid
