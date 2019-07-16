0f81665283357f006d67ba6e257938496477c3af
noah-de <noah.de@gmail.com>
2019-07-15 20:57:32 -0700

    Simplify setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-import os.path
 from setuptools import find_packages, setup
 
-# Package data
-# ------------
-_author = 'Noah Spahn'
-_author_email = 'noah.de@gmail.com'
-_classifiers = [
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author='Noah Spahn',
+    author_email='noah.de@gmail.com',
+    classifiers=[
     'Environment :: Console',
     'Intended Audience :: Developers',
     'Intended Audience :: Information Technology',
@@ -15,59 +16,16 @@ _classifiers = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules',
-]
-_description = 'SWEL'
-_download_url = 'http://pypi.python.org/pypi/swel/'
-_requirements = ["numpy"]
-_keywords = ['NOAA', 'NBDC', 'oceanography', 'earth science', 'waves']
-_license = 'Apache License, Version 2.0'
-_long_description = 'A python utility to work with raw buoy data from NOAA.'
-_name = 'swel'
-_namespaces = []
-_test_suite = 'swel.tests'
-_url = 'https://github.com/noah-de/swel'
-_version = '0.1.5.1'
-_zip_safe = False
-
-# Setup Metadata
-# --------------
-
-
-def _read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-_header = '*' * len(_name) + '\n' + _name + '\n' + '*' * len(_name)
-_longDescription = '\n\n'.join([
-    _header,
-    _read('README.md')
-])
-
-setup(
-    author=_author,
-    author_email=_author_email,
-    classifiers=_classifiers,
-    description=_description,
-    download_url=_download_url,
+],
+    description='A python utility to work with raw buoy data from NOAA.',
     include_package_data=True,
-    install_requires=_requirements,
-    keywords=_keywords,
-    license=_license,
-    long_description=_long_description,
-    name=_name,
-    namespace_packages=_namespaces,
+    keywords=['NOAA', 'NBDC', 'oceanography', 'earth science', 'waves'],
+    license='Apache License, Version 2.0',
+    long_description=long_description,
+    name='swel',
     packages=find_packages(),
-    test_suite=_test_suite,
-    url=_url,
-    version=_version,
-    zip_safe=_zip_safe,
+    test_suite='swel.tests',
+    url='https://github.com/noah-de/swel',
+    version='0.1.5.1',
+    zip_safe=False,
 )
-
-# setup(name='swel',
-#       version='0.1',
-#       description='calculate significant wave height periods from raw NOAA buoy data',
-#       url='https://github.com/noah-de/swel',
-#       author='Noah Spahn',
-#       author_email='noah.de@gmail.com	',
-#       license='MIT',
-#       packages=['swel'],
-#       zip_safe=False)
